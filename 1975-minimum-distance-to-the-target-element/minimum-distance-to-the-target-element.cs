d9f302314b90b599b164fd104efb9bc6ea771f6e
Thuy An <128052804+thuy0an@users.noreply.github.com>
2026-04-13 10:54:05 +0700

    Time: 0 ms (100.00%) | Memory: 42.3 MB (62.07%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int GetMinDistance(int[] nums, int target, int start) {
+        int n = nums.Length;
+        int res = 99999999;
+        for( int i = 0; i < n; i++)
+        {
+            if (nums[i] == target)
+            {
+                res = Math.Min(res, Math.Abs(i-start));
+            }
+        }
+        return res;
+    }
+}
