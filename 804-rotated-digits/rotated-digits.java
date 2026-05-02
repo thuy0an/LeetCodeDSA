e7de94fb4e1decde6f56cdbfea1cb3045ad7c7df
Thuy An <128052804+thuy0an@users.noreply.github.com>
2026-05-02 11:19:09 +0700

    Time: 4 ms (59.28%) | Memory: 42.7 MB (24.21%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,19 +1,27 @@
 class Solution {
     public int rotatedDigits(int n) {
-        return (int) IntStream.rangeClosed(1, n)
-                .filter(this::isGood)
-                .count();
+        int[] dp = new int[n+1];
+        int cnt = 0;
+        dp[0] =1;
+
+        for( int i = 0; i <= n; i++){
+            int a = dp[i/10];
+            int b = getDigit(i % 10);
+
+            if( a == 1 && b == 1){
+                dp[i]= 1;
+            }
+            else if ( a >= 1 && b >= 1){
+                dp[i] = 2;
+                cnt++;
+            }
+        }
+        return cnt;
     }
 
-    private boolean isGood(int num){
-        String s = String.valueOf(num);
-        boolean check = false;
-
-        for (char c : s.toCharArray()) {
-            if (c == '3' || c == '4' || c == '7') return false;
-            if (c == '2' || c == '5' || c == '6' || c == '9') check = true;
-        }
-
-        return check;
+    private int getDigit(int d){
+        if ( d == 0 || d ==  1 || d == 8) return 1;
+        else if ( d == 2 || d == 5 || d == 6 || d == 9) return 2;
+        return 0;
     }
 }
